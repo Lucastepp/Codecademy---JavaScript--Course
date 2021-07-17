77a410ae1f3c962037687e9e07fd21f7496ed039
Lucas Teixeira Pinto <85134699+Lucastepp@users.noreply.github.com>
2021-07-17 22:47:03 +0100

    Add files via upload

---
+++ JS - 002 - Dog Years.js
@@ -0,0 +1,24 @@
+
+
+const myAge = 35;
+// setting the constant age
+
+let earlyYears = 2;
+// setting variable first 2 years
+
+earlyYears *= 10.5;
+// multiplying the first 2 dog years (10.5 * 2)
+
+var laterYears = myAge - 2;
+// taking the 2 years from the first constant
+
+laterYears *= 4;
+// multiplying the later years
+
+var myAgeInDogYears = earlyYears + laterYears;
+// sum of ages
+
+var myName = 'Lucas Teixeira'.toLowerCase();
+// set my name and turn it all lower case
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`);
