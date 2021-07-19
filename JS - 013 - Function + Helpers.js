a52ca0c5605c04a4c38db3fbf0350318e8f8e724
Lucas Teixeira Pinto <85134699+Lucastepp@users.noreply.github.com>
2021-07-19 19:20:31 +0100

    Add files via upload

---
+++ JS - 013 - Function + Helpers.js
@@ -0,0 +1,16 @@
+
+
+//helper funcions
+
+function monitorCount(rows, columns) {
+    return rows * columns;
+  }
+  
+  function costOfMonitors(rows, columns) {
+    return monitorCount(rows, columns) * 200;
+  }
+  
+  const totalCost = costOfMonitors(5, 4);
+  
+  console.log(totalCost);
+  
---
+++ JS - 014 - Function Expressions.js
@@ -0,0 +1,17 @@
+
+
+
+//Function Expressions
+
+const plantNeedsWater = function(day, plantNeedsWater){
+    if (day === 'Wednesday'){
+      return true;
+  
+    }else {
+      return false;
+    }
+  }
+  
+console.log(plantNeedsWater('Tuesday'));
+//returns false
+  
