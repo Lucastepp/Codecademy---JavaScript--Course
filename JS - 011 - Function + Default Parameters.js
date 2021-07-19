19ae64c9e215ff308fac372f7ea6277f4ebb33f1
Lucas Teixeira Pinto <85134699+Lucastepp@users.noreply.github.com>
2021-07-19 19:19:33 +0100

    Add files via upload

---
+++ JS - 011 - Function + Default Parameters.js
@@ -0,0 +1,10 @@
+
+//function + default parameters
+
+function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs'){
+    console.log(`Remember to buy ${item1}`);
+    console.log(`Remember to buy ${item2}`);
+    console.log(`Remember to buy ${item3}`);
+  }
+  
+  makeShoppingList();
---
+++ JS - 012 - Function + Return.js
@@ -0,0 +1,11 @@
+
+//function + return
+
+
+function monitorCount(rows, columns){
+    return rows * columns;
+  }
+  
+  const numOfMonitors = monitorCount(5, 4);
+  
+  console.log(numOfMonitors);
