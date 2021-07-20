92612071fd2d73c7a4f53dbdc6ce98a15f33a157
Lucas Teixeira Pinto <85134699+Lucastepp@users.noreply.github.com>
2021-07-20 20:00:07 +0100

    Add files via upload

---
+++ JS - 015 - Function + Fat Arrow.js
@@ -0,0 +1,25 @@
+
+
+// Fat Arrow Function
+
+//Before
+
+/*const plantNeedsWater = function(day) {
+  if (day === 'Wednesday') {
+    return true;
+  } else {
+    return false;
+  }
+};
+*/
+
+// =>
+
+const plantNeedsWater = (day) => {
+    if (day === 'Wednesday') {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  
---
+++ JS - 016 - Function  + Concise Body Arrow.js
@@ -0,0 +1,12 @@
+
+
+// Concise Body Arrow Functions
+
+/* BEFORE 
+const plantNeedsWater = (day) => {
+  return day === 'Wednesday' ? true : false;
+};
+*/
+
+//LATER
+const plantNeedsWater = day => day === 'Wednesday' ? true : false;
