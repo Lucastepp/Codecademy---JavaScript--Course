f8352d422b89f05dee7b55cee3f8f78508d80437
Lucas Teixeira Pinto <85134699+Lucastepp@users.noreply.github.com>
2021-07-20 20:47:22 +0100

    Add files via upload

---
+++ JS - 021 - Arrays Update Elements.js
@@ -0,0 +1,19 @@
+
+
+// Update Elements
+
+
+let seasons = ['Winter', 'Spring', 'Summer', 'Fall'];
+ 
+seasons[3] = 'Autumn';
+console.log(seasons); 
+
+//Output: ['Winter', 'Spring', 'Summer', 'Autumn']
+
+
+////
+
+let groceryList = ['bread', 'tomatoes', 'milk'];
+
+groceryList[1] = 'avocados';
+
