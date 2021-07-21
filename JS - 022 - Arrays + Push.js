2aa90f04d953a341e9a53d8fc367a420e35bcc1b
Lucas Teixeira Pinto <85134699+Lucastepp@users.noreply.github.com>
2021-07-21 18:40:14 +0100

    Add files via upload

---
+++ JS - 022 - Arrays + Push.js
@@ -0,0 +1,8 @@
+
+//Array push
+
+const chores = ['wash dishes', 'do laundry', 'take out trash'];
+
+chores.push('clean toilet', 'go to sleep');
+
+console.log(chores);
---
+++ JS - 023 - Arrays + Pop.js
@@ -0,0 +1,10 @@
+
+
+//Array Pop
+
+const chores = ['wash dishes', 'do laundry', 'take out trash', 'cook dinner', 'mop floor'];
+
+chores.pop();
+
+console.log(chores);
+
