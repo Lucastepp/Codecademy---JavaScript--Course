a37595f2073bccea02b5194bbbb8f1f186f0921f
Lucas Teixeira Pinto <85134699+Lucastepp@users.noreply.github.com>
2021-07-23 20:18:06 +0100

    Add files via upload

---
+++ JS - 035 - Iterators - Map Method.js
@@ -0,0 +1,19 @@
+
+
+// .map() Method
+
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+
+
+const secretMessage = animals.map(animal => animal[0]);
+
+console.log(secretMessage.join(''));
+
+const bigNumbers = [100, 200, 300, 400, 500];
+
+
+
+const smallNumbers = bigNumbers.map(num => num/100);
+
+console.log(smallNumbers)
