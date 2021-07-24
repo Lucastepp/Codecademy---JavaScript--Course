7dde35ebed87d8f608e3037524b7a1a7a5627c40
Lucas Teixeira Pinto <85134699+Lucastepp@users.noreply.github.com>
2021-07-24 17:03:52 +0100

    Add files via upload

---
+++ JS - 041 - Objects.js
@@ -0,0 +1,14 @@
+
+//Objects --
+
+let spaceship = {
+    'Fuel Type' : 'Turbo Fuel',
+    homePlanet : 'Earth',
+    color: 'silver',
+    'Secret Mission' : 'Discover life outside of Earth.'
+  };
+  
+  spaceship.color = 'glorious gold';
+  spaceship.numEngines = 5;
+  delete spaceship['Secret Mission'];
+  
