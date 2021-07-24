4fd6909e61bcc0778fa79f8edbe5bd23ed1410e6
Lucas Teixeira Pinto <85134699+Lucastepp@users.noreply.github.com>
2021-07-24 13:52:58 +0100

    Add files via upload

---
+++ JS - 036 - Iterators - Filter Method.js
@@ -0,0 +1,17 @@
+
+
+// Filter() Method
+
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+
+const smallNumbers = randomNumbers.filter(randomNumber => {
+  return randomNumber < 250;
+});
+
+
+const favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
+
+
+const longFavoriteWords = favoriteWords.filter(favoriteWord => {
+  return favoriteWord.length > 7;
+});
