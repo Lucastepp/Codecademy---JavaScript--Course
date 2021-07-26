9a630361547fb70e0fd7b66dc97da1f4cc0e7e9b
Lucas Teixeira Pinto <85134699+Lucastepp@users.noreply.github.com>
2021-07-26 20:09:27 +0100

    Add files via upload

---
+++ JS - 046 - Objects - This Keyword.js
@@ -0,0 +1,12 @@
+
+//This keyword
+
+const robot = {
+    model: '1E78V2',
+    energyLevel: 100,
+    provideInfo() { 
+      return `I am ${this.model} and my current energy level is ${this.energyLevel}.`
+    }
+  };
+  
+  console.log(robot.provideInfo());
