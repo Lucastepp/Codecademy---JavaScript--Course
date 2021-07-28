5d52aa9537ac3e8ed293ed8e8345e322b9e8803c
Lucas Teixeira Pinto <85134699+Lucastepp@users.noreply.github.com>
2021-07-28 21:36:26 +0100

    Add files via upload

---
+++ JS - 049 - Objects - Factory Functions.js
@@ -0,0 +1,19 @@
+
+
+//Factory Functions
+
+
+const robotFactory = (model, mobile) => {
+  return {
+    model: model,
+    mobile: mobile,
+    beep(){
+      console.log('Beep Boop');
+    }
+  }
+};
+
+const tinCan = robotFactory('P-500', true);
+
+tinCan.beep();
+  
