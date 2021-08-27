fa7cc6ddf54463396a87b9cbc1cec6346fa8e540
Lucas Teixeira Pinto <85134699+Lucastepp@users.noreply.github.com>
2021-08-27 20:03:04 +0100

    Add files via upload

---
+++ JS - 064 - Promise Node setTimeout.js
@@ -0,0 +1,18 @@
+
+
+
+console.log("This is the first line of code in app.js.");
+
+
+
+const usingSTO = () => {
+  console.log('Yay! Coding is so fun!!');
+};
+
+setTimeout(usingSTO, 2000);
+
+
+
+
+
+console.log("This is the last line of code in app.js.");
