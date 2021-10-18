4026a7859b8a45c6cb455f86a602df17416e8062
Lucas Teixeira Pinto <85134699+Lucastepp@users.noreply.github.com>
2021-10-18 18:37:58 +0100

    Add files via upload

---
+++ REACT - 006 - JSX Passing a Variable to ReactDOM.render().js
@@ -0,0 +1,16 @@
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Write code here:
+const myList = (
+  <ul>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+  </ul>
+);
+
+ReactDOM.render(myList, document.getElementById('app'));
